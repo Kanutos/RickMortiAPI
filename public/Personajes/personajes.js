96dcc0c07d0370cd06276c6c0ef53200ab8ba2bc
Ignacio Bielsa <bielsa.arign22@zaragoza.salesianos.edu>
2024-06-15 11:51:49 +0200

    Maquetacion de la pestaña personajes, añadido texto, imagenes, stilo y transiciones, fase inicial y funcional

---
+++ public/Personajes/personajes.js
@@ -0,0 +1,17 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const characters = document.querySelectorAll('.character');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    characters.forEach(character => {
+        observer.observe(character);
+    });
+});
